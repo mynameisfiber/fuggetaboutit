4fdd4157bc14871694d53e572cdc26c412e34d9e
Micha Gorelick <mynameisfiber@gmail.com>
2013-10-12 17:08:17 -0400

    Cleanup + nojekyll

--- build/lib.macosx-10.7-intel-2.7/fuggetaboutit/__init__.py
+++
@@ -1,5 +0,0 @@
-from counting_bloom_filter import CountingBloomFilter
-from timing_bloom_filter import TimingBloomFilter
-from scaling_timing_bloom_filter import ScalingTimingBloomFilter
-
-__all__ = [CountingBloomFilter, TimingBloomFilter, ScalingTimingBloomFilter]
--- build/lib.macosx-10.7-intel-2.7/fuggetaboutit/test.py
+++
@@ -1,5 +0,0 @@
-from tests.test_scaling_timing_bloom_filter import TestScalingTimingBloomFilter
-from tests.test_timing_bloom_filter import TestTimingBloomFilter
-
-def all():
-    return TestTimingBloomFilter, TestScalingTimingBloomFilter
